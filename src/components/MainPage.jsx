2fea86f144a96c38b1e09309f03516b18a139909
Riley DuBord <thelifeofdu@gmail.com>
2024-07-22 17:11:08 -0500

    ripped out the vite, need to finalize

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,13 @@
+// src/components/MainPage.jsx
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <main style={{ padding: '1rem' }}>
+      <h2>Welcome to the Main Page</h2>
+      <p>This is a simple page to get you started with Vite and React.</p>
+    </main>
+  )
+}
+
+export default MainPage
